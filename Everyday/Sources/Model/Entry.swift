2d96a33cbe778b9751ea7982d996e1bb2ded9a71
ksjung <cks678928@gmail.com>
2018-07-28 16:11:47 +0900

    add-entry-model

---
+++ Everyday/Sources/Model/Entry.swift
@@ -0,0 +1,25 @@
+//
+//  Entry.swift
+//  Everyday
+//
+//  Created by Cocoa on 2018. 7. 28..
+//  Copyright © 2018년 ksjung. All rights reserved.
+//
+
+import Foundation
+
+class Entry{
+    let id: Int
+    let createdAt: Date
+    var text: String
+    
+    init(id: Int, createdAt: Date, text: String){
+        self.id = id
+        self.createdAt = createdAt
+        self.text = text
+    }
+}
+
+let newEntry = Entry(id: 0, createdAt: Date(), text: "첫 번째 일기")
+
+
