e1b33aff9fc54b372338c578e67d3ab0242bd197
ksjung <cks678928@gmail.com>
2018-09-01 10:24:52 +0900

    RealmRepository를 구현

--- Everyday/Sources/Controllers/SettingsTableViewController.swift
+++ Everyday/Sources/Controllers/SettingsTableViewController.swift
@@ -9,11 +9,7 @@
 import UIKit
 class SettingsTableViewController: UITableViewController {
     var viewModel: SettingsTableViewViewModel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("Setting Load: ", UserDefaults.standard.object(forKey: "fontSizeOptionKey"))
-    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
