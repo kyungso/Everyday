808cfea1d643269b5be3eb5aaa415b5cea58b367
ksjung <cks678928@gmail.com>
2018-09-01 10:03:53 +0900

    RealmEntry를 추가

---
+++ Everyday/Sources/Model/RealmEntry.swift
@@ -0,0 +1,26 @@
+//
+//  RealmEntry.swift
+//  Everyday
+//
+//  Created by Cocoa on 2018. 9. 1..
+//  Copyright © 2018년 ksjung. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmEntry: Object, EntryType {
+    var id: UUID {
+        guard let uuid = UUID.init(uuidString: uuidString) else { fatalError() }
+        return uuid
+    }
+    
+    @objc dynamic var uuidString: String = ""
+    @objc dynamic var createdAt: Date = Date()
+    @objc dynamic var text: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "uuidString"
+    }
+}
+
